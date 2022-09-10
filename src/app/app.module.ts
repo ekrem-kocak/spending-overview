4a8ae2e14f24a3eb926569ffb0cab90d6ddbf3d0
ekrem.kocak <ekrem-kocak@outlook.com>
2022-09-10 15:46:38 +0300

    Money module added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoneyModule } from './money/money.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MoneyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
