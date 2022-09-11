59606cbdf64275cc0af3044a1657ee6961dcb2c8
ekrem.kocak <ekrem-kocak@outlook.com>
2022-09-11 23:12:44 +0300

    Added bootstrap to the project. The registration process is included in the login component. The shared module has been created and the waiting component has been added.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  isLogin: boolean = true;
+  loading: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeLogin(status: boolean) {
+    if (status == this.isLogin) { return }
+    this.loading = true;
+    setTimeout(() => {
+      this.isLogin = status;
+      this.loading = false;
+    }, 1500);
+  }
 }
--- src/app/money/money.module.ts
+++ src/app/money/money.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MoneyRoutingModule } from './money-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MoneyRoutingModule
+    MoneyRoutingModule,
+    SharedModule
   ]
 })
 export class MoneyModule { }
